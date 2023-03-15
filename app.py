49fbe5291f7db019376a72a266ead84c782349bc
Gurvir Dua <gdua@terpmail.umd.edu>
2023-03-15 15:52:48 -0400

    Preliminary routes completed.

--- app.py
+++ app.py
@@ -5,8 +5,29 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def about():
+def home():
     return render_template('home.html');
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/collaborations')
+def collaborations():
+    return render_template('collaborations.html')
+
+@app.route('/createAccount')
+def createAccount():
+    return render_template('createAccount.html')
+
+@app.route('/events')
+def events():
+    return render_template('events.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
 if __name__ == '__main__':
     app.run()
