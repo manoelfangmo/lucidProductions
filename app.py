ff398c7b106f3c908d5b84cf2a004a96b9183108
Manoel <manoelfangmo@gmail.com>
2023-03-17 21:54:53 -0400

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	.idea/misc.xml
    #	.idea/team11spring2023bmgt407.iml
    #	app.py
    #	templates/about.html
    #	templates/collaborations.html
    
    Added Links to pages and  some fixed errors on pages

--- app.py
+++ app.py
@@ -29,5 +29,46 @@ def about():
 def reviews():
     return render_template('reviews.html');
 
+@app.route('/management')
+def management():
+    return render_template('management.html');
+
+@app.route('/management/analytics')
+def managementAnalytics():
+    return render_template('managementanalytics.html');
+@app.route('/management/inquiries')
+def managementInquiries():
+    return render_template('managementinquiries.html');
+@app.route('/management/reviews')
+def managementReviews():
+    return render_template('managementreviews.html');
+
+@app.route('/management/users')
+def managementUsers():
+    return render_template('managementusers.html');
+
+
+@app.route('/client')
+def client():
+    return render_template('client.html');
+
+@app.route('/client/form')
+def clientForm():
+    return render_template('clientform.html');
+
+@app.route('/guest')
+def guest():
+    return render_template('guest.html');
+
+@app.route('/guest/flag')
+def guestFlag():
+    return render_template('guestflag.html');
+
+@app.route('/collaborations/contractWorker')
+def contractWorker():
+    return render_template('contractWorker.html');
+
+
+
 if __name__ == '__main__':
     app.run()
