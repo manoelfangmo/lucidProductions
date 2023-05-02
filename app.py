1ced485c7612911410ae4ab17c9f10cd32b21446
endalk <endalkmakonnen@gmail.com>
2023-05-02 16:52:47 -0400

    users can now update their information, fixed nav bar

--- app.py
+++ app.py
@@ -72,7 +72,7 @@ def events():
         user_is_guest = False
         if current_user.is_authenticated and current_user.role == "GUEST":
             user_is_guest = True
-        return render_template('events/events.html', flyers=flyers, dates=dates, zip=zip, event_ids=event_ids,
+        return render_template('events/events.html', flyers=flyers, dates=dates, zipcode=zipcode, event_ids=event_ids,
                                user_is_guest=user_is_guest);
 
     else:
