6fb6e798ccabfb1c58ecc338ac4c577f7caceb15
Giles Oji <goji@umd.edu>
2023-05-04 17:55:54 -0400

    added to analytics dashboard and styled better

--- visualizations_routes.py
+++ visualizations_routes.py
@@ -29,6 +29,18 @@ def management_analytics():
         func.count(ContractWorker.contract_inquiry_id).label('number of contract work inquiries')).scalar()
     num_contract_inquiries_int = int(num_contract_inquiries)
 
+    num_management_users = db.session.query(
+        func.count(User.user_id).filter(User.role == 'ADMIN').label('number of contract work inquiries')).scalar()
+    num_management_users_int = int(num_management_users)
+
+    num_guest_users = db.session.query(
+        func.count(User.user_id).filter(User.role == 'GUEST').label('number of contract work inquiries')).scalar()
+    num_guest_users_int = int(num_guest_users)
+
+    num_client_users = db.session.query(
+        func.count(User.user_id).filter(User.role == 'CLIENT').label('number of contract work inquiries')).scalar()
+    num_client_users_int = int(num_client_users)
+
     qry_events_per_month = db.session.query(
         func.strftime('%Y-%m', Event.event_date).label('Month'),
         func.count(Event.event_id).label('Events Scheduled')
@@ -42,10 +54,11 @@ def management_analytics():
     events_per_month_fig = px.line(df_events_per_month, x="Month", y="Events Scheduled",
                                    color_discrete_sequence=px.colors.qualitative.Antique,
                                    title="Number of Events Scheduled per Month", markers=True,
-                                   labels={'Month': '', 'Events Scheduled': 'Count'})
+                                   labels={'Month': '', 'Events Scheduled': 'Events Scheduled'})
 
     events_per_month_fig.update_layout(
-        title={'x': 0.5}
+        title={'x': 0.5},
+        margin=dict(l=20, r=20, t=40, b=20)
     )
 
     events_per_month_figJSON = events_per_month_fig.to_json()
@@ -53,4 +66,6 @@ def management_analytics():
     return render_template('management/managementanalytics.html', rating=avg_rating_int,
                            num_event_inquiries=num_event_inquiries_int,
                            num_contract_inquiries=num_contract_inquiries_int,
+                           num_management_users=num_management_users_int, num_guest_users=num_guest_users_int,
+                           num_client_users=num_client_users_int,
                            events_per_month_graph=events_per_month_figJSON)
